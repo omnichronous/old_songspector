c5696db8b14a06832396d8b409cde9baf7d365c3
Mitko Georgiev <omnichronous@gmail.com>
2015-05-08 13:52:10 +0300

    Adapted for OpenShift

--- main.js
+++ main.js
@@ -156,3 +156,10 @@ function verify (output) {
 // ===
 app.listen(8080);
 console.log('App running!');
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
